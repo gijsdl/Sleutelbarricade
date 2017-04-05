1c28375d32e8ed5cc534fcf6185675eadc5b292c
gijsdl <gijs.de.lange@live.nl>
2017-04-05 15:42:35 +0200

    barricade

--- src/sleutelbarricade/Barricade.java
+++ src/sleutelbarricade/Barricade.java
@@ -10,9 +10,22 @@ package sleutelbarricade;
  * @author Gijs
  */
 public class Barricade extends Tegel {
+    private static int locationX;
+    private static int locationY;
+
+    private static int pincode;
+    private static int status;
+    private static int barricadeNummer;
     
-    public Barricade(int locationX, int locationY) {
+    public Barricade(int locationX, int locationY, int pincode, int status, int barricadeNummer) {
         super(locationX, locationY);
+         this.locationX = locationX;
+        this.locationY = locationY;
+
+        this.pincode = pincode;
+        this.status = status;
+        this.barricadeNummer = barricadeNummer;
     }
+    
     
 }
