49ead7f76402f24c9446dc7bef4f15200fef580a
gijsdl <gijs.de.lange@live.nl>
2017-04-05 15:26:08 +0200

    sleutelArray

--- src/sleutelbarricade/Sleutel_tegel.java
+++ src/sleutelbarricade/Sleutel_tegel.java
@@ -10,9 +10,34 @@ package sleutelbarricade;
  * @author Gijs
  */
 public class Sleutel_tegel extends Tegel {
+    private static int locationX;
+    private static int locationY;
 
-    public Sleutel_tegel(int locationX, int locationY, int pincode, boolean status) {
+    private static int pincode;
+    private static int status;
+    private static int sleutelNummer;
+
+    public Sleutel_tegel(int locationX, int locationY, int pincode, int status, int sleutelNummer) {
         super(locationX, locationY);
+        this.locationX = locationX;
+        this.locationY = locationY;
+
+        this.pincode = pincode;
+        this.status = status;
+        this.sleutelNummer = sleutelNummer;
+
+    }
+
+    private static int[][] sleutelBase = new int[5][4];
+
+    public static void store() {
+
+        sleutelBase[sleutelNummer][0] = locationX;
+        sleutelBase[sleutelNummer][1] = locationY;
+
+        sleutelBase[sleutelNummer][2] = pincode;
+        sleutelBase[sleutelNummer][3] = status;
+
     }
    
 }
